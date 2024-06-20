dbcb23c2f6f055add5bfd53aa192f40eaf590261
emadero <emaderotorres@gmail.com>
2024-06-20 00:33:58 -0300

    TypeChecker implementation

--- gantt.py
+++ gantt.py
@@ -2,27 +2,13 @@ import plotly.express as px
 import pandas as pd
 
 df1 = pd.DataFrame([
-    dict(Task="TareaInicial<br>(Minimum length: 2)", Days=3),
-    dict(Task="TareaIntermedia<br>(Minimum length: 3)<br>(Depends on \"TareaInicial\")", Days=5, Base=3),
+    dict(Task="Diseño de la interfaz (1 points)", Days=2, Category="Desarrollo"),
+    dict(Task="Desarrollo del backend (2 points)", Days=3, Category="Pruebas"),
+    dict(Task="Configuración del entorno de pruebas (2 points)", Days=1, Category="Pruebas"),
+    dict(Task="Ejecución de pruebas unitarias (4 points)<br>(Depends on \"Configuración del entorno de pruebas\")", Days=2, Category="Pruebas", Base=1),
+    dict(Task="Implementación de la funcionalidad principal (6 points)<br>(Depends on \"Desarrollo del backend\")", Days=4, Category="Implementación", Base=3),
 ])
 
-fig1 = px.bar(df1, x="Days", orientation="h", y="Task", title="ProyectoPadre", base="Base")
+fig1 = px.bar(df1, x="Days", orientation="h", y="Task", title="ProyectoComplejo (Start:2024-04-01) (Maximum Tasks:6)", color='Category', base="Base")
 fig1.update_yaxes(autorange="reversed")
-df2 = pd.DataFrame([
-    dict(Task="TareaHijo1<br>(Minimum length: 2)", Days=3),
-    dict(Task="TareaHijo2<br>(Minimum length: 3)<br>(Depends on \"TareaHijo1\")", Days=5, Base=3),
-    dict(Task="TareaHijo3<br>(Minimum length: 1)<br>(Depends on \"TareaHijo2\")", Days=2, Base=8),
-])
-
-fig2 = px.bar(df2, x="Days", orientation="h", y="Task", title="ProyectoHijo (Depends on \"ProyectoPadre\")", base="Base")
-fig2.update_yaxes(autorange="reversed")
-df3 = pd.DataFrame([
-    dict(Task="tareaUnion<br>(Minimum length: 2)", Days=5),
-])
-
-fig3 = px.bar(df3, x="Days", orientation="h", y="Task", title="UnionDeProyectos (With \"ProyectoPadre\", \"ProyectoHijo\")")
-fig3.update_yaxes(autorange="reversed")
-with open('gantt.html', 'w') as f:
-    f.write(fig1.to_html(full_html=False, include_plotlyjs='cdn'))
-    f.write(fig2.to_html(full_html=False, include_plotlyjs='cdn'))
-    f.write(fig3.to_html(full_html=False, include_plotlyjs='cdn'))
+fig1.write_html('gantt.html')
